0ffc60b6146464524a724e63b9a77634ae0ddad3
Samuel Duzen <156154083+SamDuzen@users.noreply.github.com>
2025-02-13 02:10:33 -0800

    Updated weights

--- Team 4/Sam Folder/weight_balance.m
+++ Team 4/Sam Folder/weight_balance.m
@@ -19,11 +19,11 @@ AR = 4.0431; %aspect ratio
 n_missile = 4; %number of missiles
 
 %General propulsion
-n_eng = 1; %number of engines being used
+n_eng = 2; %number of engines being used
 TSFC = 0.6; %Thrust specific fuel consumption (british units)
     TSFC_cruise = TSFC; %change if needed
     TSFC_dash = TSFC; %change if needed
-W_eng_o = 21796.29; %dry weight of engine [N]
+W_eng_o = 10898*2; %dry weight of engine [N]
 
 %General Aerodynamics
 Cdo_c = 0.0468; %parasitic drag at cruise
@@ -352,4 +352,123 @@ SM_Sub = (X_NP_Sub-X_CG*FL)/MAC
 SM_Super = (X_NP_Super-X_CG*FL)/MAC
 
 
+
+
+%% Start Numerical Solver [NOTE: THIS IS JUST TO TEST. EVERYTHING BELOW CAN BE DELTED]
+Wgt(1) = Gross_Weight;
+error(1) = 20000;
+i = 1;
+while abs(error(i)) > 10
+
+%% Approximate Empty Weight Buildup
+
+%Approximate table values
+W_Wing = 44*S; %Wing mass [kg]
+W_Tail_H = 20*S_HT; %Horizontal tail mass [kg]
+W_Tail_V = 20*S_VT; %Vertical tail mass [kg]
+W_Fus = 23*Sf_Wet; %Fuselage weight [kg] - FIND Sf_Wet
+
+W_LG = 0.033*Wgt(i); %Landing gear weight [N]
+W_AEE = 0.17*Wgt(i); %"All-else empty" weight [N]
+
+W_eng_i = 1.3*W_eng_o; %Installed engine weight [N]
+
+%Total weight check
+W_tot_t(i) = (W_Wing+W_Tail_H+W_Tail_V+W_Fus)*9.81 + W_LG + W_AEE + W_eng_i;
+
+%% Improved Empty Weight Fraction
+
+%Coefficients (for a jet fighter)
+EW = W_tot_t(i);
+
+%% Improved Fuel Weight Estimate
+W(1) = Wgt(i); %initial weight
+
+%Engine Start/Taxi/Takeoff
+Wfr(1) = 0.98; %historical estimate
+W(2) = Wfr(1)*W(1); %historical estimate
+Wfu(1) = (1-Wfr(1))*W(1); %fuel weight
+
+%Climb and Accelerate
+Wfr(2) = 1.0065 - 0.0325*Ma_cruise; %accelerate up to cruise
+W(3) = Wfr(2)*W(2);
+Wfu(2) = (1-Wfr(2))*W(2);
+
+%Cruise - 300nm
+R1 = 300*1852; %range in meters
+Wfr(3) = exp(-(TSFC_cruise*g*R1)/(v_cruise*L2D_cruise));
+W(4) = Wfr(3)*W(3);
+Wfu(3) = (1-Wfr(3))*W(3);
+
+%Loiter - 4hrs
+t1 = 4*60*60; %loiter time [sec]
+Wfr(4) = exp(-(TSFC_cruise*g*t1)/(L2D_cruise));
+W(5) = Wfr(4)*W(4);
+Wfu(4) = (1-Wfr(4))*W(4);
+
+%Accelerate up to max speed
+Wfr(5) = (0.991-0.007*Ma-0.01*Ma^2)/(1.0065-0.0325*Ma_cruise);
+W(6) = Wfr(5)*W(5);
+Wfu(5) = (1-Wfr(5))*W(5);
+
+%Dash - 100nm
+R2 = 100*1852; %range in meters
+Wfr(6) = exp(-(TSFC_dash*g*R2)/(v_dash*L2D_dash));
+W(7) = Wfr(6)*W(6);
+Wfu(6) = (1-Wfr(6))*W(6);
+
+%Combat
+v_turn_1 = 1.2*a_dash; %turn speed for turn 1
+v_turn_2 = 0.9*a_dash; %turn speed for turn 2
+d1 = (2*pi*v_turn_1)/(g*sqrt(n_max^2-1)); %combat duration for turn 1
+d2 = (2*pi*v_turn_2)/(g*sqrt(n_max^2-1)); %combat duration for turn 2
+d = d1+d2; %total combat duration
+Wfr(7) = 1 - TSFC_dash*g*T2W_dash*d;
+W(8) = Wfr(7)*W(7);
+    W(8) = W(8) - W_missiles; %all missiles fired
+Wfu(7) = (1-Wfr(7))*W(7);
+
+%Cruise - 400nm
+R2 = 400*1852; %range in meters
+Wfr(8) = exp(-(TSFC_cruise*g*R2)/(v_cruise*L2D_cruise));
+W(9) = Wfr(8)*W(8);
+Wfu(8) = (1-Wfr(8))*W(8);
+
+%Descent
+Wfr(9) = 0.9925; %historical estimation
+W(10) = Wfr(9)*W(9);
+Wfu(9) = (1-Wfr(9))*W(9);
+
+%Landing/Taxi
+Wfr(10) = 0.995; %historical estimation
+W(11) = Wfr(10)*W(10);
+Wfu(10) = (1-Wfr(10))*W(10);
+
+%Reserves: 30 minutes loiter
+t2 = 0.5*60*60; %loiter time [sec]
+Wfr(11) = exp(-(TSFC_cruise*g*t2)/(L2D_cruise));
+W(12) = Wfr(11)*W(11);
+Wfu(11) = (1-Wfr(11))*W(11);
+
+%Total Fuel Weight
+Wf = sum(Wfu);
+Wf = 1.06*Wf; %additional allowance for unusable fuel
+
+
+%% Gross Weight
+Wg_calct(i) = Wp+Wf+EW;
+
+%% End Numerical Solver
+error(i+1) = abs(Wg_calct(i)-Wgt(i));
+if error(i+1) < error(i)
+    Wgt(i+1) = Wgt(i)-0.05*abs(error(i+1));
+elseif error(i+1) > error(i)
+    Wgt(i+1) = Wgt(i)+0.05*abs(error(i+1));
+end
+
+i = i+1;
+end
+
+Wg_calct(end) = [];
+fprintf('The gross weight converges at %d Newtons\n',Wg_calct(end))
 
