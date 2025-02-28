07a176cff9be639926fc60c89c57eba4d7880a9d
Samuel Duzen <156154083+SamDuzen@users.noreply.github.com>
2025-02-27 19:39:45 -0800

    Cost Analysis

--- Team 4/DAPCA_IV_Model_Cost.m
+++ Team 4/DAPCA_IV_Model_Cost.m
@@ -1,6 +1,10 @@
 clear;close all;clc;
 %% define input parameters
-We = 2400; % empty weight [kg] ---> added up all of empty weights to and divided it by 9.81 m/s^2 to get kg 
+We = 2455.8; % empty weight [kg] ---> added up all of empty weights to and divided it by 9.81 m/s^2 to get kg 
+
+FT = 6; %Flight time [hours]
+Wf = 83536; %Fuel weight [N]
+Fuel_Price = 711; %Price of JP-8 fuel per metric ton (1000kg) in USD (as of Feb 28, 2025)
 
 Mach_max = 1.6;
 speed_of_sound_mps = 343; % speed of sound at sea level (m/s) - adjust for altitude
@@ -9,7 +13,7 @@ V_kmh = V_mps * 3.6; % convert to km/h
 V = V_kmh; % maximum velocity [km/h]
 
 
-Q = 150*5; % expected production quantity in five years 
+Q = 1000; % expected production quantity in five years 
 FTA = 2; % number of flight-test aircraft
 
 Neng = 2; % number of engines per aircraft
@@ -20,11 +24,11 @@ Tmax = Tmax_kN; % engine maximum thrust [kN]
 
 T_turbine_inlet = 1.6; % turbine inlet mach number
 T_max_turbine_inlet = 1800; % turbine inlet temperature [K]
-Cavionics = 1140400; % avionics cost [dollars] (From AIAA document, avionics payload costs)
+Cavionics = 1140400*Q; % avionics cost [dollars] (From AIAA document, avionics payload costs)
 
 
 % choose the material and corresponding fudge factor
-material = 'Titanium'; % example, change as needed
+material = 'Aluminum'; % example, change as needed
 switch material
     case 'Aluminum'
         fudge_factor = 1.0;
@@ -41,6 +45,9 @@ switch material
 end
 
 %% DAPCA IV Cost Model
+
+FF_General = 1.2; %General fudge factor for the DAPCA model for a more modern aircraft
+
 % engineering hours (fps units)
 He_fps = 4.86 * We^0.777 * V^0.894 * Q^0.163;
 
@@ -85,12 +92,29 @@ Ceng_fps = 3112/9.0667*Tmax + 243.25*max(1,T_turbine_inlet) + 0.9697*T_max_turbi
 
 % engine production cost (mks units)
 Ceng_mks = 3112/9.666*Tmax + 243.25*max(1,T_turbine_inlet) + 1.747*T_max_turbine_inlet -2228;
-Ceng_mks = 245000000/59; %59 Ej200 engines were sold to the Spanish Air force for 245 Million dollars in Dec 24
+Ceng_mks = (245000000/59)*Q; %59 Ej200 engines were sold to the Spanish Air force for 245 Million dollars in Dec 24
 
 %% fudge factor to relevant cost components
 Cd_mks = Cd_mks * fudge_factor; % development Support Cost
 Cm_mks = Cm_mks * fudge_factor; % manufacturing Materials Cost
 %Ceng_mks = Ceng_mks * fudge_factor; % engine Production Cost
+
+%% PRODUCTION COST 
+
+% define production quantities for each year
+production_quantities = [1:1000]; % FIX
+learning_rate = 0.8; % 80% learning curve means 20% cost reduction per doubling of production)
+
+% calculate production cost for each year using the learning curve
+for i = 1:length(production_quantities)
+  production_cost8(i) = Hm_mks * production_quantities(i)^(log10(learning_rate)/log10(2));
+  production_cost9(i) = Hm_mks * production_quantities(i)^(log10(0.9)/log10(2));
+  production_cost95(i) = Hm_mks * production_quantities(i)^(log10(0.95)/log10(2));
+  production_cost7(i) = Hm_mks * production_quantities(i)^(log10(0.7)/log10(2));
+  production_cost6(i) = Hm_mks * production_quantities(i)^(log10(0.6)/log10(2));
+end
+
+
 
 %% TOTAL 
 
@@ -101,7 +125,7 @@ Rq = 147.56;
 Rm = 133.9;
 
 % total rdt&e + flyaway cost (using mks units)
-RDTandE_flyaway = Cd_mks + Ht_mks*Rt + Cm_mks + Hm_mks*Rm + HQ*Rq + Cf_mks + 2*Ceng_mks + Cavionics + He_mks*Re;
+RDTandE_flyaway = FF_General*(Cd_mks + Ht_mks*Rt + Cm_mks + Hm_mks*Rm + HQ*Rq + Cf_mks + 2*Ceng_mks + Cavionics + He_mks*Re);
 
 %Flyaway cost 
 C_flyaway = Ht_mks*Rt + Cm_mks + Hm_mks*Rm + Ceng_mks + Cavionics;
@@ -119,24 +143,6 @@ costTable = table(Cd_mks, Ht_mks, Cm_mks, Hm_mks, HQ, Cf_mks, Ceng_mks, Cavionic
 disp(costTable)
 
 
-
-
-%% PRODUCTION COST 
-
-% define production quantities for each year
-production_quantities = [1:1000]; % FIX
-learning_rate = 0.8; % 80% learning curve means 20% cost reduction per doubling of production)
-
-% calculate production cost for each year using the learning curve
-for i = 1:length(production_quantities)
-  production_cost8(i) = Cm_mks * production_quantities(i)^(log10(learning_rate)/log10(2));
-  production_cost9(i) = Cm_mks * production_quantities(i)^(log10(0.9)/log10(2));
-  production_cost95(i) = Cm_mks * production_quantities(i)^(log10(0.95)/log10(2));
-  production_cost7(i) = Cm_mks * production_quantities(i)^(log10(0.7)/log10(2));
-  production_cost6(i) = Cm_mks * production_quantities(i)^(log10(0.6)/log10(2));
-end
-
-
 %% OPERATION AND SUPPORT COSTS (???) MAYBE GET RID OF 
 
 % Define annual operating and support costs
@@ -146,6 +152,25 @@ annual_support_costs = 5000000; % Example costs - adjust as needed
 % Calculate total operating and support costs
 total_operating_costs = sum(annual_operating_costs);
 total_support_costs = sum(annual_support_costs);
+
+% Fuel and Oil Costs
+FHPY = 400; %Flight hours per year per aircraft
+FPH = Wf/FT; %Fuel weight used per hour [N/hr]
+FuelW_Yearly = FHPY*FPH; %Fuel weight used per year per aircraft [N/yr]
+    Fuel_Total = (FuelW_Yearly/9.81)*(Fuel_Price/1000)*Q;
+
+%Crew Salaries
+CR = 1.1; %Crew Ratio
+CN = 1; %Number of crew members per aircraft
+CC = 2080*Re; %Cost per crew member
+    Crew_Total = CN*CR*CC*Q; %Cost per year for crew to operate all produced aircrafts
+
+% Maintenance
+MMHPFH = 10; %Maintenance Man Hours Per Flight Hour
+MMHPY = MMHPFH*FHPY; %Maintance Man Hours Per Year (per aircraft)
+    Maintenance_Total = 2*MMHPY*Rm*Q; %Cost per year for maintance of all produced aircrafts
+
+Operating_Total = Fuel_Total+Crew_Total+Maintenance_Total
 
 %% TOTAL LIFE CYCLE
 
